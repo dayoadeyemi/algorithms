12483f71edab0153f03f2a18774b2767ea21f465
Dayo Adeyemi <dayoadeyemi@users.noreply.github.com>
2017-02-10 18:01:16 +0000

    Update PE51.js

--- PE51.js
+++ PE51.js
@@ -46,3 +46,47 @@ while ((p = G.next()) < 1100) {
 		pset.push(p);
 	}
 }
+process.exit(1);
+"use strict";
+const primes = require('./primes.json');
+
+function matchable(p, q){
+    let x, y, d = 1, ret = '';
+    while(p > q){
+        let u = p%10, v = q%10, e = u;
+        if (u !== v) {
+            if (!Number.isInteger(x)) {
+                x = u;
+                y = v;
+            } else if (x !== u || y !== v) {
+                return false;
+            }
+            e = '*';
+        }
+        ret=e+ret;
+        p = (p-u)/10;
+        q = (q-v)/10;
+        d*=10;
+    }
+    return p ? p+ret : ret;
+}
+let cache = {};
+let max = 0;
+for (let i = 0; primes[i]; i++){
+    for (let j = 0; j < i; j++){
+        const p = primes[i], q = primes[j];
+        const m = matchable(p, q);
+        if (m) {
+            cache[m] = cache[m] ? cache[m] + 1 : 1;
+            if (cache[m] > max) {
+                console.log(m, cache[m]);
+                max = cache[m];
+                cache = {};
+            }
+            // if (cache[m] > significant_count) {
+            //     significant_count+=diff++;
+            //     console.log(m, 'n:', cache[m], significant_count);
+            // }
+        }
+    }
+}
