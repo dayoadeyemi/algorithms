8ff14329baab0e0b9cfcd62e02b0adc0be3fa865
dayoadeyemi <thedayoadeyemi@gmail.com>
2017-02-16 23:49:36 +0000

    pe76

---
+++ PE76.js
@@ -0,0 +1,38 @@
+"use strict";
+const cache = {}
+/**
+ * N = Sum to make
+ * d = no of summands
+ * m = leading summand
+ */
+const f = (N, d, m, ind) => {
+    // console.log(ind, `f(${N},${d},${m})`);
+    let s = 0;
+    let key = [N,d,m].join();
+    if (d === 1 || d === N) s = 1;
+    else if (cache[key]) {
+        // console.log(ind, '[cache hit!]')
+        s = cache[key]
+    }
+    else {
+        const minX = Math.ceil((N-m)/(d-1));
+        const maxX = Math.min(m, (N-m)-d+2);
+        // console.log(ind, minX, '<= x <=', maxX)
+        for (let x =minX; x <= maxX; x++){
+            s += f(N-m, d-1, x, ind+'  ')
+        }
+        cache[key] = s;
+    }
+    // console.log(ind, 'res', s)
+    return s;
+}
+
+function sums(N){
+    let _s = 0;
+    for(let d = 3; d<=N+1;d++) _s += f(2*N-1, d, N-1, '')
+    return _s;
+}
+
+const ans = sums(100);
+
+console.log(ans);
